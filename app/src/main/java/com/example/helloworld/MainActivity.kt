9f6f45e3d1afc2cef139e9117c6eb2c8362abe6d
dev-sonukr <sonukumarraj2005@gmail.com>
2025-02-25 23:52:46 +0530

    Second Commit
    Some Updates

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.net.Uri
-import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,23 +12,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // testing gitignore
+        val follow = findViewById<Button>(R.id.Button1)
+        val connect = findViewById<Button>(R.id.Button2)
+        val skills = findViewById<Button>(R.id.BtnSkills)
+        val education = findViewById<Button>(R.id.BtnEducation)
+
+        follow.setOnClickListener {
+            val url = "https://www.instagram.com/by_sonu_kr/"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        }
+
+        connect.setOnClickListener {
+            val url = "https://www.linkedin.com/in/dev-sonukr/"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        }
+
+        skills.setOnClickListener{
+            intent = Intent(this, SkillActivity::class.java)
+            startActivity(intent)
+        }
+
+        education.setOnClickListener {
+            intent = Intent(this,EducationActivity::class.java)
+            startActivity(intent)
+        }
     }
 
-    // Button click ke liye method
-    fun openLink(view: View) {
-        // URL define karein jo aap open karna chahte hain
+    // another button listener
+    /*fun openLink(view: View) {
         val url = "https://www.instagram.com/by_sonu_kr/"
         val intent = Intent(Intent.ACTION_VIEW)
         intent.data = Uri.parse(url)
         startActivity(intent)
-    }
-
-    fun click(view: View) {
-        val url = "https://www.linkedin.com/in/sonukumar2005/"
-        val intent = Intent(Intent.ACTION_VIEW)
-        intent.data = Uri.parse(url)
-        startActivity(intent)
-    }
-
+    }*/
 }
